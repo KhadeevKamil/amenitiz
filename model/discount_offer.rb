49559f2cad5d38e121f732e32b46e220dcde6210
Kamil Khadeev <kml.khadeev@gmail.com>
2023-12-19 23:01:29 +0000

    update offer service and add offers.json

--- model/discount_offer.rb
+++ model/discount_offer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Discount Offer
-class CoffeeDiscountOffer < Offer
+class DiscountOffer < Offer
   def apply_discount(products)
     # Logic for coffee discount
   end
--- service/offer_service.rb
+++ service/offer_service.rb
@@ -1,5 +1,43 @@
 # frozen_string_literal: true
 
 class OfferService
+  def initialize(json_file)
+    @offers = load_offers_from_json(json_file)
+  end
 
+  def apply_offers(cart_items)
+    cart_items.group_by(&:code).each do |code, items|
+      offer = find_offer_for_product(code)
+      offer&.apply_discount(items)
+    end
+  end
+
+  private
+
+  def load_offers_from_json(json_file)
+    file = File.read(json_file)
+    offers_data = JSON.parse(file)
+
+    offers_data.map do |offer_data|
+      offer_type, product_code = offer_data.to_a.first
+      [product_code, create_offer_instance(offer_type)]
+    end.to_h
+  end
+
+  def find_offer_for_product(product_code)
+    @offers[product_code]
+  end
+
+  def create_offer_instance(offer_type)
+    case offer_type
+    when 'bogo_offer'
+      BogofOffer.new
+    when 'bulk_discount'
+      BulkDiscountOffer.new
+    when 'discount'
+      DiscountOffer.new
+    else
+      nil
+    end
+  end
 end
