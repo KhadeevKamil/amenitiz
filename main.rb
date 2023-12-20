e454e55722f1c3b082e02bfb268ea9d2ea7df996
Kamil Khadeev <kml.khadeev@gmail.com>
2023-12-20 02:48:04 +0000

    add main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
-require_relative 'builder/product_builder'
+require_relative 'service/checkout_service'
 
-product = ProductBuilder.product(code: "GR1")
-puts product.name
+checkout_service = CheckoutService.new
+puts "Welcome to the Checkout System"
+puts "Please enter product codes separated by commas (e.g., GR1,SR1,GR1) or type 'DONE' to finish:"
+
+loop do
+  input = gets.chomp
+  break if input.upcase == 'DONE'
+
+  product_codes = input.split(',').map(&:strip)
+  product_codes.each do |code|
+    checkout_service.scan(code)
+    puts "#{code} added to your cart."
+  end
+
+  puts "Current Total: #{'%.2f' % checkout_service.total}€"
+  puts "Enter more product codes or 'DONE' to finish:"
+end
+
+total = checkout_service.total
+puts "Final Total: #{'%.2f' % total}€"
