9825fc103443c31486c1d8d3f5a0693c46a6206d
Kamil Khadeev <kml.khadeev@gmail.com>
2023-12-20 03:13:41 +0000

    The sum for the entered items is output

--- main.rb
+++ main.rb
@@ -1,24 +1,27 @@
-# frozen_string_literal: true
-
 require_relative 'service/checkout_service'
 
-checkout_service = CheckoutService.new
 puts "Welcome to the Checkout System"
-puts "Please enter product codes separated by commas (e.g., GR1,SR1,GR1) or type 'DONE' to finish:"
 
 loop do
+  puts "Please enter product codes separated by commas (e.g., GR1,SR1,GR1), or type 'DONE' to finish:"
   input = gets.chomp
   break if input.upcase == 'DONE'
 
+  # Create a new instance of CheckoutService for each input
+  checkout_service = CheckoutService.new
+
   product_codes = input.split(',').map(&:strip)
   product_codes.each do |code|
-    checkout_service.scan(code)
-    puts "#{code} added to your cart."
+    begin
+      checkout_service.scan(code)
+      puts "#{code} added to your cart."
+    rescue RuntimeError => e
+      puts "Error: #{e.message}"
+    end
   end
 
-  puts "Current Total: #{'%.2f' % checkout_service.total}€"
-  puts "Enter more product codes or 'DONE' to finish:"
+  total = checkout_service.total
+  puts "Total for entered products: #{'%.2f' % total}€"
 end
 
-total = checkout_service.total
-puts "Final Total: #{'%.2f' % total}€"
+puts "Checkout finished."
